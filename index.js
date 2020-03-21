2b0ac08d606d01b7bafb5c0356c68860a6d06ad9
Josh <joshwallacemp@gmail.com>
2020-03-21 19:44:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,375 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const RichEmbed = require('discord.js');
+
+
+
+
+//-----------------
+//EMBEDS
+//-----------------
+//Timer
+
+let clock;
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "!timer 180")
+    if(message.member.roles.some(r => r.name === "Timer"))
+    
+    {   clearInterval(clock);
+        message.channel.send("180 SECONDS ON THE CLOCK")
+        let timer = 180;
+        clock = setInterval(() => {
+            timer--;
+            console.log(timer);
+            if (timer == 180)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 90)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 60)
+            {
+                message.channel.send({embed :{
+                    color: 15105570,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 30)
+            {
+                message.channel.send({embed :{
+                    color: 15105570,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 10)
+            {
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 0)
+            {
+                clearInterval(clock);
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: "...TIME..."
+                }})
+            }
+        }, 1000) 
+      }
+    
+});
+client.on("message", message => {
+    if(message.content.toLowerCase() == "!timer 90")
+    if(message.member.roles.some(r => r.name === "Timer"))    
+    {   clearInterval(clock);
+        message.channel.send("90 SECONDS ON THE CLOCK")
+        let timer = 90;
+        clock = setInterval(() => {
+            timer--;
+            console.log(timer);
+            if (timer == 60)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 45)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 30)
+            {
+                message.channel.send({embed :{
+                    color: 15105570,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 15)
+            {
+                message.channel.send({embed :{
+                    color: 15105570,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 5)
+            {
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 0)
+            {
+                clearInterval(clock);
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: "...TIME..."
+                }})
+            }
+        }, 1000) 
+      }
+    
+});
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "!timer 60")
+    if(message.member.roles.some(r => r.name === "Timer"))
+        {   clearInterval(clock);
+        message.channel.send("60 SECONDS ON THE CLOCK")
+        let timer = 60;
+        clock = setInterval(() => {
+            timer--;
+            console.log(timer);
+            if (timer == 45)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 30)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 25)
+            {
+                message.channel.send({embed :{
+                    color: 15105570,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 15)
+            {
+                message.channel.send({embed :{
+                    color: 15105570,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 10)
+            {
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 5)
+            {
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: timer + " Seconds left"
+                }})
+            }
+            if(timer == 0)
+            {
+                clearInterval(clock);
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: "...TIME..."
+                }})
+            }
+        }, 1000) 
+      }
+    
+});
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "!timer 45")
+    if(message.member.roles.some(r => r.name === "Timer"))
+        {   clearInterval(clock);
+        message.channel.send("45 SECONDS ON THE CLOCK")
+        let timer = 45;
+        clock = setInterval(() => {
+            timer--;
+            console.log(timer);
+            if (timer == 45)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 30)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 20)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            
+            if(timer == 10)
+            {
+                message.channel.send({embed :{
+                    color: 15105570,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 5)
+            {
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 0)
+            {
+                clearInterval(clock);
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: "...TIME..."
+                }})
+            }
+        }, 1000) 
+      }
+    
+});
+
+
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "!timer 30")
+    if(message.member.roles.some(r => r.name === "Timer"))
+    
+    
+    {   clearInterval(clock);
+        message.channel.send("30 SECONDS ON THE CLOCK")
+        let timer = 30;
+        clock = setInterval(() => {
+            timer--;
+            console.log(timer);
+                       
+            if (timer == 30)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 25)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 20)
+            {
+                message.channel.send({embed :{
+                    color: 3066993,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 15)
+            {
+                message.channel.send({embed :{
+                    color: 15105570,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 10)
+            {
+                message.channel.send({embed :{
+                    color: 15105570,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer == 5)
+            {
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: timer +  " Seconds Left"
+                }})
+            }
+            if(timer  == 0)
+            {
+                clearInterval(clock);
+                message.channel.send({embed :{
+                    color: 15158332,
+                    title: "...TIME..."
+                }})
+            }
+        }, 1000) 
+      }
+    });
+
+
+    client.on("message", message => {
+        if(message.content.toLowerCase() == "!" + "timerstop")
+        {
+            clearInterval(clock);
+            message.channel.send({embed :{
+                color: 15158332,
+                title: "TIMER STOPPED"
+            }})
+        }
+            
+            }); 
+
+
+//--------------------
+//Playing a game :
+client.on("ready", () => {
+    let user = "Made with ESH by STeaMie"
+        client.user.setActivity(user)
+  });
+
+//----------------------------------------------------------------
+//Flip a coin
+function coinFlip(){
+    return (Math.floor(Math.random()* 2 ) == 0) ? 'Tails!' : 'Heads!';
+}
+client.on('message', message =>{
+   if(message.content.toLowerCase() == '!' + 'flip'){
+       message.channel.send("It's" + " " + coinFlip() + "!");
+   }
+});
+//------------------------------------------------------------------
+
+
+client.on('message', message => {
+    if(message.content.toLowerCase() === '!' + 'help') {
+        message.channel.send({embed: {
+            color: 3447003,
+            description: "Here are the commands:\n```Coin Flip : !flip \nTimer : !timer 180, 90, 60, 45 or 30 \nStop Timer : !timerstop \nTimer commands require Timer role```"
+        }});
+        
+    }
+});
+
+
+///-----------------------------------------------------------------
+
+client.on('ready',function(){
+        console.log("Ready");
+});
+
+
+
+
+
+
+client.login(process.env.TOKEN);
